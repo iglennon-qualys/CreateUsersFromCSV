013714bf53dc3e374b32c5551ba0402aea7f32a0
glennog <ianglennon@gmail.com>
2022-03-09 21:07:51 +0000

    Completed fixes and using a configuration file to define the CSV consumption rules, as well as the record skipping rules.

--- create_users_from_csv.py
+++ create_users_from_csv.py
@@ -37,6 +37,7 @@ class QualysUser:
     external_id: str
     asset_groups: list
     business_unit: str
+    time_zone_code: str
 
     def create_url(self, baseurl: str, send_email: bool = False, user_role: str = 'reader'):
         url = '%s/msp/user.php' % baseurl
@@ -52,8 +53,12 @@ class QualysUser:
             'country': self.country,
             'user_role': user_role,
             'asset_groups': ','.join(self.asset_groups),
-            'business_unit': self.business_unit
+            'business_unit': self.business_unit,
+            'time_zone_code': self.time_zone_code
         }
+        if self.external_id is not None:
+            payload['external_id'] = self.external_id
+
         if send_email:
             payload['send_email'] = '1'
         else:
@@ -65,6 +70,7 @@ class QualysUser:
 if __name__ == '__main__':
     # Add arguments to be collected from the command line
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Configuration file name (default: config.ini)')
     parser.add_argument('-f', '--filename', help='Input CSV file containing user information')
     parser.add_argument('-e', '--no_send_email', action='store_true', help='Do Not Send Email for user registration')
     parser.add_argument('-u', '--username', help='Qualys Username')
@@ -78,6 +84,14 @@ if __name__ == '__main__':
 
     # Process the passed arguments
     args = parser.parse_args()
+
+    # Validate the configuration file
+    config_file = 'config.ini'
+    if args.config is not None:
+        if os.path.exists(args.config) and os.path.isfile(args.config):
+            config_file = args.config
+        else:
+            my_quit(1, 'Config file does not exist or is not a file: %s' % args.config)
 
     # Validate the critical arguments
     if args.filename is None or args.filename == '':
@@ -107,47 +121,155 @@ if __name__ == '__main__':
     else:
         password = args.password
 
+    # Process the configuration file
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    if 'COLUMNS' not in config.keys():
+        my_quit(1, '[COLUMNS] section not found in configuration file %s' % config_file)
+
+    if 'OVERRIDE' not in config.keys():
+        my_quit(1, '[OVERRIDE] section not found in configuration file %s' % config_file)
+
+    if 'SKIP_RECORDS' not in config.keys():
+        my_quit(1, '[SKIP_RECORDS] section not found in configuration file %s' % config_file)
+
+    config_default = config['COLUMNS']
+    config_override = config['OVERRIDE']
+    config_skip = config['SKIP_RECORDS']
+
+    for i in ['forename', 'surname', 'email', 'title', 'phone', 'address1', 'city', 'country', 'asset_groups',
+              'time_zone_code', 'external_id', 'business_unit']:
+        if i not in config_default.keys() and i not in config_override.keys():
+            my_quit(1, 'Configuration item \'%s\' not found in configuration file' % i)
+
     if args.proxy_enable:
         api = QualysAPI.QualysAPI(svr=args.apiurl, usr=args.username, passwd=password, enableProxy=args.proxy_enable,
                                   proxy=args.proxy_url, debug=args.debug)
     else:
         api = QualysAPI.QualysAPI(svr=args.apiurl, usr=args.username, passwd=password, debug=args.debug)
 
+    skip_records = False
+    skip_record_column = 9999999
+    skip_record_indicator = None
+    if 'skip_records' in config_skip.keys():
+        if config_skip['skip_records'].lower() == 'true':
+            skip_records = True
+            if 'skip_record_indicator' not in config_skip.keys() or 'skip_record_column' not in config_skip.keys():
+                my_quit(1, '[SKIP_RECORDS] section incomplete, must include \'skip_record_indicator\' and '
+                           '\'skip_record_column\'')
+            skip_record_indicator = config_skip['skip_record_indicator'].split(',')
+            skip_record_column = int(config_skip['skip_record_column'])
+
     with open(args.filename, 'r') as inputfile:
         csvreader = csv.reader(inputfile, delimiter=',', quotechar='"')
 
         for row in csvreader:
-            if row[11] == 'OK':
+
+            if skip_records and row[skip_record_column] not in skip_record_indicator:
                 user = QualysUser()
-                user.forename = row[1]
-                user.surname = row[2]
-                user.email = row[3]
-                user.title = row[4]
-                user.phone = row[5]
-                user.address1 = row[6]
-                user.city = 'London'
-                user.country = row[7]
-                user.asset_groups = row[9].split(';')
-                user.external_id = row[3]
-                user.business_unit = 'Unassigned'
+                if 'forename' in config_override.keys():
+                    user.forename = config_override['forename']
+                else:
+                    if config_default['forename'] == 'override':
+                        my_quit(1, 'Override not found: \'forename\'')
+                    user.forename = row[int(config_default['forename'])]
+
+                if 'surname' in config_override.keys():
+                    user.surname = config_override['surname']
+                else:
+                    if config_default['surname'] == 'override':
+                        my_quit(1, 'Override not found: \'surname\'')
+                    user.surname = row[int(config_default['surname'])]
+
+                if 'email' in config_override.keys():
+                    user.email = config_override['email']
+                else:
+                    if config_default['email'] == 'override':
+                        my_quit(1, 'Override not found: \'email\'')
+                    user.email = row[int(config_default['email'])]
+
+                if 'title' in config_override.keys():
+                    user.title = config_override['title']
+                else:
+                    if config_default['title'] == 'override':
+                        my_quit(1, 'Override not found: \'title\'')
+                    user.title = row[int(config_default['title'])]
+
+                if 'phone' in config_override.keys():
+                    user.phone = config_override['phone']
+                else:
+                    if config_default['phone'] == 'override':
+                        my_quit(1, 'Override not found: \'phone\'')
+                    user.phone = row[int(config_default['phone'])]
+
+                if 'address1' in config_override.keys():
+                    user.address1 = config_override['address1']
+                else:
+                    if config_default['address1'] == 'override':
+                        my_quit(1, 'Override not found: \'address1\'')
+                    user.address1 = row[int(config_default['address1'])]
+
+                if 'city' in config_override.keys():
+                    user.city = config_override['city']
+                else:
+                    if config_default['city'] == 'override':
+                        my_quit(1, 'Override not found: \'city\'')
+                    user.city = row[int(config_default['city'])]
+
+                if 'country' in config_override.keys():
+                    user.country = config_override['country']
+                else:
+                    if config_default['country'] == 'override':
+                        my_quit(1, 'Override not found: \'country\'')
+                    user.country = row[int(config_default['country'])]
+
+                if 'asset_groups' in config_override.keys():
+                    user.asset_groups = config_override['asset_groups'].split(';')
+                else:
+                    if config_default['asset_groups'] == 'override':
+                        my_quit(1, 'Override not found: \'asset_groups\'')
+                    user.asset_groups = row[int(config_default['asset_groups'])].split(';')
+
+                if 'external_id' in config_override.keys():
+                    user.external_id = config_override['external_id']
+                else:
+                    if config_default['external_id'] == 'override':
+                        my_quit(1, 'Override not found: \'external_id\'')
+                    user.external_id = row[int(config_default['external_id'])]
+
+                if 'business_unit' in config_override.keys():
+                    user.business_unit = config_override['business_unit']
+                else:
+                    if config_default['business_unit'] == 'override':
+                        my_quit(1, 'Override not found: \'business_unit\'')
+                    user.external_id = row[int(config_default['business_unit'])]
+
+                if 'time_zone_code' in config_override.keys():
+                    user.time_zone_code = config_override['time_zone_code']
+                else:
+                    if config_default['time_zone_code'] == 'override':
+                        my_quit(1, 'Override not found: \'time_zone_code\'')
+                    user.time_zone_code = row[int(config_default['time_zone_code'])]
 
                 url, payload = user.create_url(baseurl=api.server, send_email=not args.no_send_email,
                                                user_role='reader')
-
-                response = api.makeCall(url=url, payload=payload)
-                error_code, error_message = validate_api_response(response=response)
-                if error_code > 0 and args.exit_on_error:
-                    my_quit(exitcode=error_code, errormsg='Could not create user %s %s (%s) : Reason (%s)' %
-                                                          (user.forename,
-                                                           user.surname,
-                                                           user.email,
-                                                           error_message))
-                elif error_code > 0:
-                    print('ERROR: Could not create user %s %s (%s) : Reason (%s)' % (user.forename, user.surname,
-                                                                                     user.email, error_message))
+                if not args.no_call:
+                    response = api.makeCall(url=url, payload=payload)
+                    error_code, error_message = validate_api_response(response=response)
+                    if error_code > 0 and args.exit_on_error:
+                        my_quit(exitcode=error_code, errormsg='Could not create user %s %s (%s) : Reason (%s)' %
+                                                              (user.forename,
+                                                               user.surname,
+                                                               user.email,
+                                                               error_message))
+                    elif error_code > 0:
+                        print('ERROR: Could not create user %s %s (%s) : Reason (%s)' % (user.forename, user.surname,
+                                                                                         user.email, error_message))
+                    else:
+                        username = response.find('.//USER_LOGIN').text
+                        print('User %s (%s %s) Created' % (username, user.forename, user.surname))
                 else:
-                    username = response.find('.//USER_LOGIN').text
-                    print('User %s (%s %s) Created' % (username, user.forename, user.surname))
-
+                    print('NO_CALL: Output URL and Payload\nURL: %s\nPAYLOAD: %s' % (url, payload))
             else:
-                print('Skipping INCOMPLETE entry for %s %s' % (row[1], row[2]))
+                print('Skipping %s entry for %s %s' % (row[skip_record_column], row[1], row[2]))
--- readme.md
+++ readme.md
@@ -16,46 +16,33 @@
 * class QualysAPI
 
 ## Usage
-
-Create Qualys user accounts from user information stored in a CSV file
-The CSV file has the following column order
-<unread value>, forename, surname, email, job title, phone, address1, country, asset_groups*
-
-*asset groups are semi-colon delimited
-
-example:
-
 ```
-<unread_value>, John, Smith, johnsmith@email.com, Teacher, 1234, Farringham Road, England, <unread_value>, TAR;DIS, <unread_value>, OK
-```
-* The external_id value will be set to the same as the email address
-* The business unit of the user account will be set to 'Unassigned'
-* The city of the user will be set to 'London'
-* If the item in column 11 of any given row is not 'OK', the row will be skipped
+usage: create_users_from_csv.py [-h] [-c CONFIG] [-f FILENAME] [-e] [-u USERNAME] [-p PASSWORD] [-P] [-U PROXY_URL] [-a APIURL] [-n] [-d] [-x]
 
-```
-usage: create_users_from_csv.py [-h] [-f FILENAME] [-e] [-u USERNAME] [-p PASSWORD] [-P] [-U PROXY_URL] [-a APIURL] [-n] [-d] [-x]
-
-options:                                                                                                                          
-  -h, --help            show this help message and exit                                                                           
-  -f FILENAME, --filename FILENAME                                                                                                
-                        Input CSV file containing user information                                                                
-  -e, --no_send_email   Do Not Send Email for user registration                                                                   
-  -u USERNAME, --username USERNAME                                                                                                
-                        Qualys Username                                                                                           
-  -p PASSWORD, --password PASSWORD                                                                                                
-                        Qualys Password (use - for interactive input                                                              
-  -P, --proxy_enable    Enable HTTPS proxy                                                                                        
-  -U PROXY_URL, --proxy_url PROXY_URL                                                                                             
-                        HTTPS proxy address                                                                                       
-  -a APIURL, --apiurl APIURL                                                                                                      
-                        Qualys API URL (e.g. https://qualysapi.qualys.com)                                                        
-  -n, --no_call         Do not make API calls, write URLs to console                                                              
-  -d, --debug           Provide debugging output from API calls                                                                   
-  -x, --exit_on_error   Exit on error                                                                                             
+options:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Configuration file name (default: config.ini)
+  -f FILENAME, --filename FILENAME
+                        Input CSV file containing user information
+  -e, --no_send_email   Do Not Send Email for user registration
+  -u USERNAME, --username USERNAME
+                        Qualys Username
+  -p PASSWORD, --password PASSWORD
+                        Qualys Password (use - for interactive input
+  -P, --proxy_enable    Enable HTTPS proxy
+  -U PROXY_URL, --proxy_url PROXY_URL
+                        HTTPS proxy address
+  -a APIURL, --apiurl APIURL
+                        Qualys API URL (e.g. https://qualysapi.qualys.com)
+  -n, --no_call         Do not make API calls, write URLs to console
+  -d, --debug           Provide debugging output from API calls
+  -x, --exit_on_error   Exit on error
+                                                                                             
 ```
 
 ## Example
+inputfile.csv
 ```csv
 1, Joan, Redfern, joan.redfern@farringham.school, Nurse, 1234, Farringham Road, United Kingdom, SICKBAY, foo, OK
 2, Timothy, Latimer, timothy.latimer@farringham.school, Student, 1234, Farringham Road, United Kingdom, BEER, foo, OK
@@ -63,10 +50,10 @@ options:
 4, Mister, Clark, mister.clark@oakham.farm, Father, 1234, Oakham Farm, United Kingdom, FAMILY;FATHER, foo, INCOMPLETE
 5, Mister, Rocastle, headmaster@farringham.school, Headmaster, 1234, Farringham Road, United Kingdom, SCHOOL, NOT_OK
 ```
-
+<br>
 
 ```commandline
-python -f inputfile.csv -e -u redfr-td -p - -P -U https://proxy.address -a https://qualysapi.qualys.eu --no_call --debug --exit_on_error
+python -f inputfile.csv -e -u redfr-td -p - -P -U https://proxy.address -a https://qualysapi.qualys.eu
 Enter password : 
 
 User redfr-ar45 (Joan Redfern) Created
@@ -75,3 +62,73 @@ User redfr-eb45 (Jeremy Baines) Created
 Skipping INCOMPLETE entry for Mister Clark
 Skipping INCOMPLETE entry for Mister Rocastle
 ```
+
+## Input files
+
+Create Qualys user accounts from user information stored in a CSV file, in accordance with the rules set out in the
+configuration file ('config.ini' by default)
+
+### Configuration File
+Example
+```
+[SKIP_RECORDS]
+skip_records = TRUE
+skip_record_indicator = INCOMPLETE,NOT_OK
+skip_record_column = 11
+
+[OVERRIDE]
+city = London
+time_zone_code = GB
+business_unit = Unassigned
+
+[COLUMNS]
+forename = 1
+surname = 2
+email = 3
+title = 4
+phone = 5
+address1 = 6
+country = 8
+asset_groups = 9
+external_id = 3
+```
+The configuration file **must** include the sections [SKIP_RECORDS], [OVERRIDE] and [COLUMNS] and **must** include the
+following fields in either the [OVERRIDE] or [COLUMNS] sections
+* forename
+* surname
+* email
+* title
+* phone
+* address1
+* city
+* country
+* asset_groups
+* time_zone_code
+* external_id
+* business_unit
+
+If a field is specified in both [OVERRIDE] and [COLUMNS] sections, the [OVERRIDE] section takes priority
+
+#### [SKIP_RECORDS]
+This section handles whether a skip_record indicator is included in the CSV file.
+
+For record skipping to be enabled, skip_records must be set to 'TRUE'.
+
+Once enabled, both the skip_record_indicator column must be present (defined by 'skip_record_column') and must contain 
+one of the items in the comma-separated list of indicators (defined by 'skip_record_indicator').
+
+#### [OVERRIDE]
+Columns in the CSV can be overridden with static data.  For overrides to occur, the field name must appear in this
+section.  This allows a common value to be applied to all records in the CSV.
+
+#### [COLUMNS]
+This section gives the column number of the CSV to use for each required field, starting with 0 for the first column.
+
+### CSV File
+
+Example:
+
+```
+<unread_value>, John, Smith, johnsmith@email.com, Teacher, 1234, Farringham Road, England, <unread_value>, TAR;DIS, <unread_value>, OK
+```
+
